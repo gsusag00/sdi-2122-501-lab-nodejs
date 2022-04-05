1fb5f2d4f0926fda66a0b6f4539347f8ce32b5ef
jesusag00 <jesus.ag00@gmail.com>
2022-04-05 10:37:51 +0200

    SDI-2122-501-9.4-Sistema de compras.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -34,10 +34,13 @@ app.set('crypto',crypto);
 
 app.use("/songs/add",userSessionRouter);
 app.use("/publications",userSessionRouter);
+app.use("/songs/buy",userSessionRouter);
+app.use("/purchases",userSessionRouter);
 app.use("/audios/",userAudiosRouter);
 app.use("/shop/",userSessionRouter);
 app.use("/songs/edit",userAuthorRouter);
 app.use("/songs/delete",userAuthorRouter);
+
 
 
 
